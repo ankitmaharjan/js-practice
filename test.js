59c5642c2886e36842bf5cf77f310afad965304c
Ankit Maharjan <ankit.maharjan@swifttech.com>
2023-04-10 23:34:40 -0700

    changes

--- test.js
+++ test.js
@@ -1,9 +1,42 @@
+
+// object hell
 const obj = 
 {
-    name:"swift",
+    name:()=>
+    {
+        const ob2 = {
+
+            ankit: ()=>
+            {
+
+            }
+        }
+    },
     phone:"98555222",
     signature:"rftgyhuj"
 }
 
 
-console.log(`Welcome to ${obj.name}`);
+
+//object example
+const obj1 =
+{
+    name :"abishek",
+    age:20
+}
+console.log(`Welcome to ${obj1.name}`);
+
+
+
+//array operations
+
+let arr = [ "banana", "mango", "apple"];
+
+arr.pop(1);
+
+console.log(arr);
+
+
+let arr12 = [ "banana", "mango", "apple"];
+arr.push("avhishek","Royal challenger banglore");
+console.log(arr);
